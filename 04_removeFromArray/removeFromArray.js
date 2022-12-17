94db980de1392b69ce6885794f38155ca39a5d70
John <jmhearn@hmc.edu>
2022-12-16 19:35:45 -1000

    finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...toRemove) {
+  let arrLen = arr.length;
+  let newArr = [];
+  for (let i = 0; i < arrLen; i++) {
+    let remove = false;
+    for (const e of toRemove) {
+      if (arr[i] === e) {
+        remove = true;
+        break;
+      }
+    }
+    if (!remove) newArr.push(arr[i]);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
