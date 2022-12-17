6a2b5b81b1d4acab9d5a9e7dc93da05e14d4771c
John <jmhearn@hmc.edu>
2022-12-16 19:43:30 -1000

    finished sumAll\

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (n, m) {
+  if (m < 0 || n < 0 || typeof m != "number" || typeof n != "number")
+    return "ERROR";
+  if (n > m) return sumAll(m, n);
+  let sum = 0;
+  for (let i = n; i <= m; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
