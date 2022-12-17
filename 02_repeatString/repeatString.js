10105b7417dea0d3ac9550705ada6996f4da0e8b
John <jmhearn@hmc.edu>
2022-12-16 18:58:26 -1000

    finished repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function a(s, num) {
+  if (num < 0) return "ERROR";
+  let newString = "";
+  for (let i = 0; i < num; i++) {
+    newString = newString.concat(s);
+  }
+  return newString;
 };
 
 // Do not edit below this line
