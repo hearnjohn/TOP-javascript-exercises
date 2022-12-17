eb9b8fffc4d305cf1ec29fff40db1bd0f1a45fc8
John <jmhearn@hmc.edu>
2022-12-16 19:03:44 -1000

    finished reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (s) {
+  let sLen = s.length;
+  let result = "";
+  for (let i = 0; i < sLen; i++) {
+    result = result.concat(s[sLen - i - 1]);
+  }
+  return result;
 };
 
 // Do not edit below this line
