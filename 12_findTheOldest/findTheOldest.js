d3851bb581b779edc1793c319607a3d6c5ea8d7e
John <jmhearn@hmc.edu>
2022-12-22 08:49:38 -1000

    findTheOldest works in console.log but not with returns

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.forEach((person) => {
+    if (!person.yearOfDeath) person.yearOfDeath = 2022;
+  });
+  let sorted = people.sort((a, b) =>
+    a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1
+  );
+  console.log(sorted[0].name);
 };
 
 // Do not edit below this line
