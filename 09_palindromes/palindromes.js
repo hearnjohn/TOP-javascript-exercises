f12558c994d9375abf5ba460929b97972d094e8c
John <jmhearn@hmc.edu>
2022-12-22 08:13:30 -1000

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  let lP = 0,
+    rP = s.length - 1;
+  while (lP <= rP) {
+    if (!isAlpha(s[lP])) {
+      lP++;
+      continue;
+    } else if (!isAlpha(s[rP])) {
+      rP--;
+    } else {
+      if (s[lP].toLowerCase() !== s[rP].toLowerCase()) return false;
+      ++lP;
+      --rP;
+    }
+  }
+  return true;
 };
 
+function isAlpha(ch) {
+  return /^[A-Z]$/i.test(ch);
+}
 // Do not edit below this line
 module.exports = palindromes;
