890711c8ff6922ac63eaaec4156d780bff438ab8
John <jmhearn@hmc.edu>
2022-12-22 08:21:18 -1000

    fib complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (typeof n === "string") {
+    n = parseFloat(n);
+  }
+  if (n < 0) return "OOPS";
+  let twoAgo = 0,
+    oneAgo = 1;
+  if (n === 0) return twoAgo;
+  if (n === 1) return oneAgo;
+  n--;
+  while (n--) {
+    let temp = oneAgo;
+    oneAgo += twoAgo;
+    twoAgo = temp;
+  }
+  return oneAgo;
 };
 
 // Do not edit below this line
